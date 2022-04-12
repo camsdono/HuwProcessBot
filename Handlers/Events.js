188fa824308a0c5565a3d93fbcd13faa2c6c4925
camsdono <68128526+camsdono@users.noreply.github.com>
2022-04-12 15:33:44 +0100

    Add files via upload

--- Handlers/Events.js
+++ Handlers/Events.js
@@ -5,21 +5,21 @@ const PG = promisify(glob);
 const Ascii = require("ascii-table");
 
 module.exports = async (client) => {
-    const Table = new Ascii("events loaded ;)");
+    const Table = new Ascii("events loaded");
 
     (await PG (`${process.cwd()}/Events/*/*.js`)).map(async (file) => {
         const event = require(file);
         
         if(!Events.includes(event.name) || !event.name) {
-            const L =file.split("/");
-            await Table.addRow(`${event.name || "MISSING"}`, `the event does not exist you fuck: ${L[6] + `/` + L[7]}`);
+            const L = file.split("/");
+            await Table.addRow(`${event.name || "MISSING"}`, `the event does not exist you fu**: ${L[6] + `/` + L[7]}`);
             return; 
         }
 
         if(event.once) {
             client.once(event.name, (...args) => event.execute(...args, client));
         } else {
-            client.on(event.name, (...args) => event.execute(...args, client))
+            client.on(event.name, (...args) => event.execute(...args, client));
         };
 
         await Table.addRow(event.name, "it worked :D:D:D:D:D:D:D:D")
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const { Client } = require('discord.js');
+const Discord = require('discord.js')
+const { Client } = require('discord.js')
+const { Token } = require('./config.json');
 
-const client = new Client({intents: 8});
-const { Token } = require("./config.json")
+const client = new Discord.Client({
+    intents: [
+        131071
+    ]
+})
 
-require("./Handlers/Events")(client);
 
+
+
+// For error handling, makes it so your bot doesnt crash when you get an error with a command (not needed)
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+	
+	
+});
+
+require("./Handlers/Events")(client)
 
 client.login(Token)
